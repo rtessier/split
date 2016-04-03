af3981ba2a01bba76fbcfbd7d7ff6cc2874ff14f
Ryan Tessier <rtessier@Ryans-MacBook-Pro.local>
2016-04-03 19:07:22 +1000

    Only include other housemates; fix typo

--- components/HousematesList.js
+++ components/HousematesList.js
@@ -4,9 +4,9 @@ import Housemate from './Housemate';
 class HousematesList extends Component {
   render() {
     let defaultHousemates = [
-      { name: 'Ryan', amount: 150.00 },
       { name: 'Link', amount: 0.00 },
-      { name: 'Gannon', amount: -50.00 },
+      { name: 'Link', amount: 0.00 },
+      { name: 'Ganon', amount: -50.00 },
     ]
     return (
       <div>
