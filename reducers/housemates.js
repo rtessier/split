ff2d9cae5b16d1b91da5438480590a39a9024063
Ryan Tessier <rtessier@Ryans-MacBook-Pro.local>
2016-04-03 20:00:53 +1000

    Add housemates reducer

---
+++ reducers/housemates.js
@@ -0,0 +1,11 @@
+const defaultHousemates = [
+  { name: 'Link', amount: 0.00 },
+  { name: 'Ganon', amount: 0.00 },
+  { name: 'Zelda', amount: 0.00 }
+]
+
+function reduceHousemates(state = defaultHousemates, action) {
+  return state
+}
+
+export default reduceHousemates
